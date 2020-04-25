bfe0002a50a18c01acd58ad416c13bfb6d23b0f1
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2020-04-25 19:01:03 +0900

    add: 指定ラベルのIssueのCSVエクスポーター

---
+++ get_issues_csv.rb
@@ -0,0 +1,60 @@
+#! /usr/bin/env ruby
+
+require 'octokit'
+require "pry" # 開発用
+require "csv"
+require "dotenv"
+
+Dotenv.load
+
+# 仕様
+#
+# 指定ラベルのIssueをCSVにエクスポート
+#
+# @param 0 repository: リポジトリ
+# @param 1 label: 集計するラベル名
+# @param 2 ignore_labels: 除外ラベル名(カンマ区切り)
+#
+# ruby ./get_issues_csv.rb <repository_name> <from> <to> <label> <ignore_labels>
+
+if ARGV.count != 2 && ARGV.count != 3
+  puts "引数が違います"
+  puts "ruby ./get_reviewed.rb <token> <repository_name> <label> <ignore_labels>"
+  puts "ex: ruby ./get_reviewed.rb tokentokentoken username/repository_name label_name"
+  puts "ex: ruby ./get_reviewed.rb tokentokentoken username/repository_name label_name 'ignore1, ignore2'"
+  exit
+end
+
+token = ENV['GITHUB_TOKEN']
+repository = ARGV[0]
+label_name = ARGV[1]
+ignore_labels = []
+ignore_labels = ARGV[2].split(',') unless ARGV[2].nil?
+
+rows = [
+  ['number', 'state', 'title', 'body', 'url', 'labels', 'created_at', 'closed_at']
+]
+
+client = Octokit::Client.new(access_token: token)
+client.auto_paginate = true
+
+client.issues(repository, labels: label_name, state: 'all', sort: 'updated', direction: 'desc').each do |issue|
+  next unless issue.pull_request.nil?
+  labels = issue.labels.map{ |l| l[:name] }
+
+  rows << [
+    issue.number,
+    issue.state,
+    issue.title,
+    issue.body,
+    issue.html_url,
+    labels,
+    issue.created_at,
+    issue.closed_at
+  ]
+
+end
+
+csv = CSV.open('tmp/issues_' + label_name + '_' + Time.now.strftime('%Y%m%d') + '.csv','w')
+rows.each { |r| csv.puts r }
+csv.close
